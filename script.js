cdddf49bcdd43b7ad878b14aecf8773b4a5f22ec
sdlima4025 <sergiolimafg3@gmail.com>
2023-06-09 10:10:31 -0300

    Methodo GET e POST

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function clickCalback() {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then((response) => {
+        // console.log(`Status: ${response.status}`);
         return response.json();
     })
     .then((json)=> {
@@ -18,7 +19,28 @@ function clickCalback() {
     // alert("Alert 2 função executa antes da promise!");
 
 };
-
+// Enviando dados via Methodo POST
+function inserir() {
+    fetch('https://jsonplaceholder.typicode.com/posts',
+    {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: 'Título de teste',
+            body: 'Methodo POST',
+            userId: 2
+        })
+    })
+.then((response)=> {
+    return response.json();
+})
+.then((json)=> {
+    console.log(json);
+})
+}
+document.querySelector('#inserir').addEventListener('click', inserir);
 document.querySelector('#botao')
 .addEventListener('click', clickCalback);
 
