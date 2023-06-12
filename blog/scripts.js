59096d2c647492e380ecf9b189351c9e1045f5ee
sdlima4025 <sergiolimafg3@gmail.com>
2023-06-12 10:53:23 -0300

    criado evento de click no botão e requisição POST.

--- blog/scripts.js
+++ blog/scripts.js
@@ -8,11 +8,54 @@ async function readPosts() {
     let json = await response.json();
 
     if(json.length > 0) {
+        postArea.innerHTML = '';
+
+        for(let i in json) {
+            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr></div>`;
+            postArea.innerHTML += postHtml;
+        }
+
 
     }else {
         postArea.innerHTML = 'nenhum post para apresentar';
     }
-}   
+} 
+// Requisição POST
+async function addNewPost(title, body) {
+    await fetch(
+
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                userId:2
+            })
+        }
+    );
+    document.querySelector('#titleField').value = '';
+    document.querySelector('#bodyField').value = '';
+
+    readPosts()
+
+}
+
+// Evento de click no botão
+document.querySelector('#insertButton').addEventListener('click', () => {
+   let title = document.querySelector('#titleField').value;
+   let body = document.querySelector('#bodyField').value;
+
+   if(title && body) {
+        addNewPost(title, body);
+
+   }else {
+    alert('Preencha todos os campos!');
+   }
+}) 
 
 
 readPosts();
